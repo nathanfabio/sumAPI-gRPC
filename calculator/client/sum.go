9d4fe2db58d21b64d1b95340b085f83e28d251d5
Nathan Fabio <nathan_3_m@hotmail.com>
2024-02-12 10:48:22 -0300

    feat: add client side and build files

---
+++ calculator/client/sum.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/nathanfabio/sumAPI-gRPC/calculator/proto"
+)
+
+
+func doSum(c pb.CalculatorServiceClient) {
+	log.Println("doSum was invoked")
+	res, err := c.Sum(context.Background(), &pb.SumRequest{
+		FirstNumber: 1,
+		SecondNumber: 1,
+	})
+
+	if err != nil {
+		log.Fatalf("Could not sum: %v\n", err)
+	}
+
+	log.Printf("Sum: %d\n", res.Result)
+}
